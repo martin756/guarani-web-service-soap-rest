09c14689941f8bf8f70c855d7074e4c7ebe6f290
walter lacoste <wlacoste@outlook.com>
2022-10-06 03:03:46 -0300

    readme servicio rest admin

--- admin/src/main/java/plataforma/admin/controller/TestController.java
+++ admin/src/main/java/plataforma/admin/controller/TestController.java
@@ -1,21 +1,13 @@
 package plataforma.admin.controller;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import plataforma.admin.models.*;
 import plataforma.admin.services.*;
-import plataforma.admin.services.Empleado;
-
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.jws.*;
-import javax.xml.ws.*;
 
 
 @RestController
@@ -27,44 +19,10 @@ public class TestController {
     @Autowired TurnoService turnoService;
     @Autowired CatedraService catedraService;
     @Autowired MateriaService materiaService;
-
     @Autowired Generador generador;
 
     Logger logger = LoggerFactory.getLogger(TestController.class);
 
-//    @GetMapping("/insertarAlumnos")
-//    public void insertarAlumnos(){
-//        List<Usuario> users = generador.generarUsuarios();
-//        for (Usuario u: users) {
-//            usuarioService.guardarUsuario(u);
-//            logger.info("Usuario "+ u.toString());
-//        }
-//    }
-//
-//    @GetMapping("/crearMaterias")
-//    public List<Materia> crearMaterias(){
-//        return generador.getCarreras();
-//    }
-//
-//
-//
-//    @GetMapping("/getUsuario/{id}")
-//    public Usuario insertarAlumnos(@PathVariable int id){
-//        Usuario user = usuarioService.getUsuario(id);
-//        logger.info("Usuario obtenido"+ user.toString());
-//        return user;
-//
-//    }
-
-//    @GetMapping("/materias")
-//    public List<Materia> getMaterias(){
-//        Cuatrimestre cuatri = Generador.generarCuatrimestre();
-//        cuatriService.guardarCuatrimestre(cuatri);
-//        for (Materia m: cuatri.materias) {
-//            logger.info(m.toString());
-//        }
-//        return cuatri.materias;
-//    }
 
     @GetMapping("/generacionDatos")
     public List<Catedra> generacionDatos(){
@@ -85,45 +43,7 @@ public class TestController {
         List<Turno> turnos = turnoService.getTurnos();
 
         return generador.generarCatedras();
-
     }
-
-//    @GetMapping("/guardaMaterias")
-//    public String guardaMaterias(){
-//        Carrera carrera = generador.getCarrera();
-//        logger.info(carrera.toString());
-//        List<Materia> materias = new ArrayList<>();
-//        materias.add(new Materia("Farmaco", 1, carrera));
-//        materias.add(new Materia("Embrio", 2, carrera));
-//        materias.add(new Materia("Gineco", 3, carrera));
-////        carrera.materias = materias;
-//        carreraService.guardarCarrera(carrera);
-//        return "Hola";
-//    }
-//
-//    @GetMapping("/updateMateria")
-//    public String updateMateria(){
-//        Carrera carrera = carreraService.getCarrera(5);
-//        logger.info(carrera.toString());
-//        carrera.nombre = "Medicina";
-//        carreraService.updateCarrera(carrera);
-//        return "se obtienen materias";
-//    }
-//
-//    @GetMapping("/getTurnos")
-//    public List<Turno> turnos(){
-//        List<Turno> turnos = turnoService.getTurnos();
-//        return turnos;
-//    }
-//
-//    @GetMapping("/generarCuatri")
-//    public String generarCuatri(){
-//        Cuatrimestre cuatri = new Cuatrimestre();
-//        cuatri.anio = 2022;
-//        cuatri.periodo = 2;
-//        cuatriService.guardarCuatrimestre(cuatri);
-//        return "cuatriGenerado";
-//    }
 
     @GetMapping("/generarCatedra")
     public String generarCatedra(){
@@ -134,34 +54,8 @@ public class TestController {
         catedra.cuatrimestre = cuatriService.getCuatrimestre(1);
         catedra.id = new CatedraId(catedra.turno, catedra.profesor, catedra.materia, catedra.cuatrimestre);
         catedraService.guardarCatedra(catedra);
-
         return catedra.toString();
     }
-
-//    @GetMapping("/catedra")
-//    public String getCatedra(){
-//        return catedraService.getCatedra(1).toString();
-//    }
-
-//    @GetMapping("/turnos")
-//    public String getturnos(){
-//        return turnoService.getTurno(1).toString();
-//    }
-//
-//
-//
-//    @GetMapping("/cuatri")
-//    public Cuatrimestre getCuatri(){
-//        return generador.generarCuatrimestre();
-//    }
-//
-//    @GetMapping("/helloworld")
-//    public String helloworld(){
-//        Endpoint.publish("http://localhost:8081/empleadoservice", new Empleado());
-//
-//        return "ok";
-//    }
-
 
 
 
